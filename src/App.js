473cd50529f77aadec2a42f3e2800b10d5a7cb46
Minkyung Kim <min@Minkyungui-MacBookPro.local>
2020-08-06 15:07:08 +0900

    Add Material-UI & Fetch API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import youtube from './api/youtube'
+import SeachBar from './components/SearchBar'
+import VideoDetails from './components/VideoDetails'
 
 class App extends Component {
+
+    handleSubmit = async ( searchTerm ) => {
+        const response = await youtube.get('search', {  params: {
+            //part=snippet
+            part: 'snippet',
+            maxResults: 5, 
+            key: [API keys]
+            q : searchTerm
+        }})
+        // console.log( response )
+        console.log( response.data.items)
+    }
+
     render() {
+        
         return (
-            <div>
-                Youtube Clone
-            </div>
+            <Grid justify="center" container spacing={1}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={12}> 
+                            <SeachBar onFormSubmit={this.handleSubmit}/>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <VideoDetails/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            {/* Video lists */}
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         )
     }
 }
---
+++ src/components/VideoDetails.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VideoDetails() {
+    return (
+        <h1> This is from VideoDetails component </h1>
+    )
+}
+
+export default VideoDetails
